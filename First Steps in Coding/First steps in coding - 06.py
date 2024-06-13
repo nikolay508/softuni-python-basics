800dbf6cd58b65e3d44a6566df4620709ac0cecd
Nikolay Todorov <157305036+nikolay508@users.noreply.github.com>
2024-06-14 00:49:05 +0300

    Update First steps in coding - 06.py

--- First Steps in Coding/First steps in coding - 06.py
+++ First Steps in Coding/First steps in coding - 06.py
@@ -2,6 +2,7 @@ nailon = float(input())
 boq = float(input())
 razreditel = int(input())
 hours_workers = int(input())
+
 sum_nai = (nailon * 1.5) + 3
 first_sum_boq = boq * 14.5
 second_sum_boq = 0.1 * first_sum_boq
@@ -9,4 +10,5 @@ final_sum_boq = first_sum_boq + second_sum_boq
 sum_raz = razreditel * 5
 all_materials = sum_nai + final_sum_boq + sum_raz + 0.4
 workers_salary = (0.3 * all_materials) * hours_workers
+
 print(all_materials + workers_salary)
